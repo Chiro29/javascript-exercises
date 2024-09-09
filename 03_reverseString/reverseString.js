05aaaf9b3b8589931535aab9fa7cc164afb2265b
chiro <80062293+Chiro29@users.noreply.github.com>
2024-09-09 14:41:08 +0200

    fix(03-js): fixed reverseString function and completed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  const array = string.split('');
 
+  string = array.toReversed();
+  string = string.join('');
+  return string;  
 };
 
 // Do not edit below this line
