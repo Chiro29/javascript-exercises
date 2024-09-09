70f947d028588d7b9b75b46d6eaab8727d0d850e
chiro <80062293+Chiro29@users.noreply.github.com>
2024-09-09 14:16:55 +0200

    fix(02-js): fixed the repeatString function and completed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, n) {
+  let string = '';
 
+  if(n < 0) {
+    string = 'ERROR';  
+  }
+  else {
+    for(let i = 1; i <= n; i++) {
+      string = string + word;    
+    }
+  }
+
+  return string;
 };
 
 // Do not edit below this line
