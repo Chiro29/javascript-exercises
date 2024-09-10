110db7904efdc9e405bdc0880ee4ba951538fcbc
chiro <80062293+Chiro29@users.noreply.github.com>
2024-09-10 15:54:54 +0200

    fix(05-js): fixed sum function and completed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let sum = 0;
+  if ((Number.isInteger(num1) && Number.isInteger(num2)) && (num1 > 0 && num2 > 0)) {
+    if (num1 === num2) {
+      return num1;
+    }
+    else if (num1 > num2) {
+      let x = num2;
+      sum = num2;
+  
+      do {
+        x = x + 1;
+        sum = sum + x;
+      } while (x !== num1);
+    }
+    else {
+      let x = num1;
+      sum = num1;
+  
+      do {
+        x = x + 1;
+        sum = sum + x;
+      } while (x !== num2);
+    }    
+  }
+  else {
+    return "ERROR";
+  }
 
+  return sum;
 };
 
 // Do not edit below this line
