188b6b22ca6c9f441632abe7abf97be1a1d44cec
chiro <80062293+Chiro29@users.noreply.github.com>
2024-09-10 14:55:44 +0200

    fix(04-js): fixed function remove Array and completed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argArray) {
+  for(let i = array.length - 1; i >= 0; i--) {
+    for(let j = array.length - 1; j >= 0; j--) {
+      if(argArray[i] === array[j]) {
+        array = array.toSpliced(j, 1);
+      }
+    }
+  }
+
+  return array;
 };
 
 // Do not edit below this line
