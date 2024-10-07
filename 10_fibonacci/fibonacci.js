7ac2c1a2befbc3be29d5e913dbe351a418906f6a
chiro <80062293+Chiro29@users.noreply.github.com>
2024-10-07 16:52:45 +0200

    feat(10-js): completed fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  const array = [1];
+  let sum = 1;
 
+  if (n == 0)
+    return 0;  
+  if (n < 0) 
+    return "OOPS";
+
+  for (let i = 1; i < n ; i++) {
+    array.push(sum);
+    sum = array[array.length - 2]  + array[array.length - 1];
+  }
+
+  return array[array.length - 1];
 };
 
 // Do not edit below this line
