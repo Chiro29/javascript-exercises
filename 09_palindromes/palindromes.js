f6f190dfc2a2647aedb0192f16b18c0b78355512
chiro <80062293+Chiro29@users.noreply.github.com>
2024-10-07 12:41:28 +0200

    feat(09-js): Completed palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const array = string
+    .toLowerCase()
+    .split("")
+    .filter(element => /^[a-z0-9]$/.test(element))
+    .join("");
 
+  const reverseString = array
+    .split("")
+    .reverse()
+    .join("");
+
+  return array === reverseString;
+
+  // for(let i = 0; i < array.length; i++) {
+  //   for (let j = array.length - 1; j >= 0; j--) {
+  //     if (array[i] !== array[j]) {
+  //       return false;
+  //     }
+  //     else i++;
+  //   }
+  // }
+  // return true;
 };
 
 // Do not edit below this line
