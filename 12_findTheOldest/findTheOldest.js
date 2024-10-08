7a2e07a179de7b94ce828f2d19681273f7b5ad58
chiro <80062293+Chiro29@users.noreply.github.com>
2024-10-08 18:25:25 +0200

    feat(12-js): completed findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
-
+// Best solution
+const getAge = function (person) {
+  person.yearOfDeath ??= new Date().getFullYear();
+  return person.yearOfDeath - person.yearOfBirth;
 };
+
+const findTheOldest = function (people) {
+  const orderedPeople = people.toSorted((person, nextPerson) =>   getAge(nextPerson) - getAge(person));
+
+  return orderedPeople[0];
+};
+
+// My solution 
+// const findTheOldest = function(array) {
+//   const d = new Date();
+//   const oldestArray = array.sort((a, b) => {
+//     firstGuy = (a.yearOfDeath || d.getFullYear()) - a.yearOfBirth;
+//     lastGuy = (b.yearOfDeath || d.getFullYear()) - b.yearOfBirth;
+//     return firstGuy > lastGuy ? -1 : 1;
+//   });
+
+//   return oldestArray[0];
+// };
 
 // Do not edit below this line
 module.exports = findTheOldest;
